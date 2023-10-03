8dab0f75b3bfb99ac311c732b22c59c6b61112f2
ar.gayazov <>
2023-10-03 08:25:23 +0300

    fix

--- vars/build_tests.groovy
+++ vars/build_tests.groovy
@@ -1,7 +1,16 @@
 
+def get_product_project(product_name) {
+
+    all_products = [
+    'big_geek_tests': 'https://github.com/UATCO/big_geek_tests',
+    ]
+
+    return all_products[product]
+}
+
 def build(product, tests_path) {
 
-    product_git = products.get_product_project(product)
+    product_git = get_product_project(product)
 
     node {
     stage('project_update')
