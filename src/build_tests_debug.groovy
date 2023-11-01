f3b76b5f2f43335fd6ccb7efd3e57eb7322483a4
ar.gayazov <>
2023-11-01 16:16:06 +0300

    f

--- src/build_tests_debug.groovy
+++ src/build_tests_debug.groovy
@@ -54,7 +54,7 @@ def build(product, tests_path) {
 
         def command_copy = """
         chcp 65001
-        xcopy ${project_tests_path}  ${job_tests_path} /e /y /s
+        xcopy ${project_tests_path}  ${job_tests_path} /e /y /f
         copy ${job_tests_path}\\config\\show-config.ini ${job_tests_path}\\config.ini
         """
         bat(script: command_copy)
